ba8a519de80ffb93b8144757d121c84bbe7551a5
elboj <adaja.tobi1@gmail.com>
2024-07-25 17:03:06 +0100

    added module for NSG

--- modules/network-security-group/main.tf
+++ modules/network-security-group/main.tf
@@ -22,5 +22,5 @@ resource "azurerm_network_security_group" "network_security_group" {
 
 resource "azurerm_subnet_network_security_group_association" "nsg_association" {
   subnet_id                 = var.subnet_id
-  network_security_group_id = var.network_security_group_id
+  network_security_group_id = var.azurerm_network_security_group.id
 }
